759e2a538b90b710a2471f1cb3f90f18154f2649
coderaiser <coderaiser@cloudcmd.io>
2026-04-06 19:35:00 +0300

    fix: bin: palabra

--- bin/palabra.js
+++ bin/palabra.js
@@ -1,11 +1,12 @@
+#!/usr/bin/env node
 import {join} from 'node:path';
 import process from 'node:process';
 import {execa} from 'execa';
 import {tryToCatch} from 'try-to-catch';
-import {create} from '../lib/palabras.js';
+import {create} from '../lib/palabra.js';
 
 const [arg] = process.argv.slice(2);
-const name = join(process.cwd(), '.palabras.json');
+const name = join(process.cwd(), '.palabra.json');
 
 const {default: palabras} = await import(name, {
     with: {
@@ -18,7 +19,10 @@ const cmd = await create(palabras);
 if (arg !== '-q')
     console.log(`> ${cmd}`);
 
-const [error] = await tryToCatch(execa, cmd, {shell: '/bin/bash', stdio: 'inherit'});
+const [error] = await tryToCatch(execa, cmd, {
+    shell: '/bin/bash',
+    stdio: 'inherit',
+});
 
 if (error)
     process.exitCode = error.exitCode;
