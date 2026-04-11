902ac0a9f4839cfcf09359b45383abe6ac650e31
coderiaser <coderaiser@cloudcmd.io>
2026-04-11 22:46:22 +0300

    chore: actions: palabra run

---
+++ test/run-all-bin.js
@@ -0,0 +1,30 @@
+import {readdir} from 'node:fs/promises';
+
+const isString = (a) => typeof a === 'string';
+const {keys} = Object;
+const names = await readdir(new URL('../letras', import.meta.url));
+
+const bins = [];
+
+for (const name of names) {
+    const {default: letra} = await import(new URL(`../letras/${name}/${name}.json`, import.meta.url), {
+        with: {
+            type: 'json',
+        },
+    });
+    
+    if (!letra.bin)
+        continue;
+    
+    if (isString(letra.bin)) {
+        bins.push(`type ${letra.bin}`);
+        continue;
+    }
+    
+    for (const bin of keys(letra.bin)) {
+        bins.push(`type ${bin}`);
+    }
+}
+
+console.log(bins.join(' && '));
+
